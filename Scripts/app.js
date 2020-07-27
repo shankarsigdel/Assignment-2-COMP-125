c04fc1ddac2ac5f0380113b2e96be09f9fd679b5
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-07-27 19:13:10 -0400

    initial commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,137 +1,94 @@
 "use strict";
-
 // IIFE -Immediately Ivoked Function Expression
-(function(){
-
-    function highlightActiveLink(id) 
-    {
+(function () {
+    function highlightActiveLink(id) {
         let navAnchors = document.querySelectorAll("li a");
-
-        for (const anchor of navAnchors) 
-        {
-         anchor.className = "nav-link";
+        for (let _i = 0, navAnchors_1 = navAnchors; _i < navAnchors_1.length; _i++) {
+            let anchor = navAnchors_1[_i];
+            anchor.className = "nav-link";
         }
-
-        for (const anchor of navAnchors) 
-        {
+        for (let _a = 0, navAnchors_2 = navAnchors; _a < navAnchors_2.length; _a++) {
+            let anchor = navAnchors_2[_a];
             let anchorString = anchor.getAttribute("id");
-
-            if (id === anchorString)
-            {
+            if (id === anchorString) {
                 anchor.className = "nav-link active";
             }
         }
     }
-
-    function validateForm()
-    {
+    function validateForm() {
         let contact = new objects.Contact();
-
         let contactForm = document.forms[0];
-
         contactForm.noValidate = true;
-
         let errorMessage = document.getElementById("errorMessage");
-
         let firstName = document.getElementById("firstName");
-        firstName.addEventListener("blur", (event) => 
-        {
-            if(firstName.value.length <= 2)
-            {
+        firstName.addEventListener("blur", function (event) {
+            if (firstName.value.length <= 2) {
                 firstName.focus();
                 errorMessage.hidden = false;
-                errorMessage.textContent = "Invalid!! Enter more than 2 characters"; 
+                errorMessage.textContent = "Invalid!! Enter more than 2 characters";
             }
-            else
-            {
+            else {
                 contact.firstName = firstName.value;
                 errorMessage.hidden = true;
             }
         });
-
         let lastName = document.getElementById("lastName");
-        lastName.addEventListener("blur", (event) => 
-        {
-            if(lastName.value.length <= 2)
-            {
+        lastName.addEventListener("blur", function (event) {
+            if (lastName.value.length <= 2) {
                 lastName.focus();
                 errorMessage.hidden = false;
-                errorMessage.textContent = "Invalid! -- Enter more than 2 Characters!"; 
+                errorMessage.textContent = "Invalid! -- Enter more than 2 Characters!";
             }
-            else
-            {
+            else {
                 contact.lastName = lastName.value;
                 errorMessage.hidden = true;
             }
         });
-
         let contactNumber = document.getElementById("contactNumber");
-        contactNumber.addEventListener("blur", (event) =>
-        {
+        contactNumber.addEventListener("blur", function (event) {
             let contactNumberPattern = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
-            if(!contactNumberPattern.test(contactNumber.value))
-            {
+            if (!contactNumberPattern.test(contactNumber.value)) {
                 contactNumber.focus();
                 errorMessage.hidden = false;
-                errorMessage.textContent = "Invalid!! -- Please enter a Valid Contact Number"; 
+                errorMessage.textContent = "Invalid!! -- Please enter a Valid Contact Number";
             }
-            else
-            {
+            else {
                 contact.contactNumber = contactNumber.value;
                 errorMessage.hidden = true;
             }
-            
         });
-
         let emailAddress = document.getElementById("emailAddress");
-        emailAddress.addEventListener("blur", (event) =>
-        {
+        emailAddress.addEventListener("blur", function (event) {
             let emailPattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-            if(!emailPattern.test(emailAddress.value))
-            {
+            if (!emailPattern.test(emailAddress.value)) {
                 emailAddress.focus();
                 errorMessage.hidden = false;
-                errorMessage.textContent = "Invalid!! -- Please enter a Valid email address"; 
+                errorMessage.textContent = "Invalid!! -- Please enter a Valid email address";
             }
-            else
-            {
+            else {
                 contact.emailAddress = emailAddress.value;
                 errorMessage.hidden = true;
             }
-            
         });
-
         let shortMessage = document.getElementById("shortMessage");
-        shortMessage.addEventListener("blur", (event) => {
+        shortMessage.addEventListener("blur", function (event) {
             contact.shortMessage = shortMessage.value;
         });
-        
         // creates a "hook" or reference to the button element with an id of "submitButton"
         let submitButton = document.getElementById("submitButton");
-
-        submitButton.addEventListener("click", (event) =>
-        {
+        submitButton.addEventListener("click", function (event) {
             event.preventDefault();
             console.log("Submit Button Clicked");
-
             console.log(contact.toString());
-
             console.log(contact.toJSON());
-
         });
     }
-
-    function setPageContent(id)
-    {
+    function setPageContent(id) {
         document.title = id;
-
-        window.history.pushState("", id, "/"+id.toLowerCase());
-
+        window.history.pushState("", id, "/" + id.toLowerCase());
         highlightActiveLink(id);
-
-         // content switcher
-        switch(id)
-        {
+        // content switcher
+        switch (id) {
             case "Home":
                 HomeContent();
                 break;
@@ -141,141 +98,101 @@
             case "Project":
                 ProjectContent();
                 break;
-            
         }
-
         loadFooter();
     }
-
-    function InitializeSite()
-    {
+    function InitializeSite() {
         console.info("Header");
-
         // step 1 - creates the XHR object
         let XHR = new XMLHttpRequest();
-
         // step 2 - configures the message
         XHR.open("GET", "./Views/partials/header.html");
-
         // step 3 - Executes the request
         XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
+        XHR.addEventListener("readystatechange", function () {
+            if ((XHR.readyState === 4) && (XHR.status === 200)) {
                 let header = document.getElementsByTagName("header")[0];
-
                 let headerData = XHR.responseText;
-
                 header.innerHTML = headerData;
-
                 setPageContent("Home");
-
                 let navLinks = document.getElementsByTagName("a");
-
-                for (const link of navLinks) 
-                {
-                    link.addEventListener("click", (event) =>{
+                let _loop_1 = function (link) {
+                    link.addEventListener("click", function (event) {
                         event.preventDefault();
-
-                        let id = link.getAttribute("id");
-
+                       let id = link.getAttribute("id");
                         setPageContent(id);
-
                     });
+                };
+                for (let _i = 0, navLinks_1 = navLinks; _i < navLinks_1.length; _i++) {
+                    let link = navLinks_1[_i];
+                    _loop_1(link);
                 }
             }
         });
     }
-
-    function loadFooter()
-    {
-        console.info("Footer");
-
+    function loadFooter() {
+        console.info();
         // step 1 - creates the XHR object
         let XHR = new XMLHttpRequest();
-
         // step 2 - configures the message
         XHR.open("GET", "./Views/partials/footer.html");
-
         // step 3 - Executes the request
         XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
+        XHR.addEventListener("readystatechange", function () {
+            if ((XHR.readyState === 4) && (XHR.status === 200)) {
                 let footer = document.getElementsByTagName("footer")[0];
-
                 let footerData = XHR.responseText;
-
                 footer.innerHTML = footerData;
             }
         });
     }
-
-    function ContactContent()
-    {
+    function ContactContent() {
         console.info("Contact");
-
         // step 1 - creates the XHR object
         let XHR = new XMLHttpRequest();
-
         // step 2 - configures the message
         XHR.open("GET", "./Views/content/contact.html");
-
         // step 3 - Executes the request
         XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
+        XHR.addEventListener("readystatechange", function () {
+            if ((XHR.readyState === 4) && (XHR.status === 200)) {
                 let main = document.getElementsByTagName("main")[0];
-
                 let mainData = XHR.responseText;
-
                 main.innerHTML = mainData;
-
                 validateForm();
             }
         });
     }
-
-    function HomeContent()
-    {
+    function HomeContent() {
         console.info("Home");
-
         // step 1 - creates the XHR object
         let XHR = new XMLHttpRequest();
-
         // step 2 - configures the message
         XHR.open("GET", "/Views/content/home.html");
-
         // step 3 - Executes the request
         XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
+        XHR.addEventListener("readystatechange", function () {
+            if ((XHR.readyState === 4) && (XHR.status === 200)) {
                 let main = document.getElementsByTagName("main")[0];
-
                 let mainData = XHR.responseText;
-
-                main.innerHTML =  `
+                main.innerHTML = `
                 <br>  
                 <p align="center">
                 <h1 align="Center" background-color= blue;>MINI-PORTFOLIO</h1>
                 </p>
+
                 <p align="left">
                     <img width="200" height="230"src="./images/shankar.jpg">
-                    
                 </p>            
                 <p align="center">
                     <h2 align="Center" background-color= blue;>INTRODUCTION</h2>
                 </p>
+
                 <p>
                 My Name is SHANKAR SIGDEL, Student ID is 301110925, The Course Code is COMP125-004. I Live in Scarborough, Ontario. I am a student of Centennial College, Software Engineering Technology, Second Semester. Software Engineering Technology is my second career of my education. My aim after the completion of this subject is to be established in the same field. I am entertaining this subject and professor as well. Some code references for this project are taken from class lectures of Prof. Tom .IT is really an interesting field to work in. I am expecting to entertain in this field in the future.
                 </p>
                 <h2 align="Center"> MISSION STATEMENT</h2>
+                
                 <p>
                 Gain Knowledge on Technology and Work as A Leading Professinal with Sound Knowledge so that i can bring Change and Work with Innovative Ideas of Technology in the Work Place.
                 </p>
@@ -285,66 +202,50 @@
             }
         });
     }
-
-    function ProjectContent()
-    {
+    function ProjectContent() {
         console.info("Project");
-
         // step 1 - creates the XHR object
         let XHR = new XMLHttpRequest();
-
         // step 2 - configures the message
         XHR.open("GET", "./Views/content/project.html");
-
         // step 3 - Executes the request
         XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
+        XHR.addEventListener("readystatechange", function () {
+            if ((XHR.readyState === 4) && (XHR.status === 200)) {
                 let main = document.getElementsByTagName("main")[0];
-
                 let mainData = XHR.responseText;
+                main.innerHTML =
+                `                
+                <p align="left">
+                <h1 align="Center">LIST OF THE THREE PROJECTS</h1>
+                <br>
+                <h2>Project-One</h2>
+                This picture shows the pictures of the project that i have completed at Cencol.
+                <img src="./images/project1.png"style="width:100%">
+                </p>
 
-                main.innerHTML = 
-                    `                
-                    <p align="left">
-                    <h1 align="Center">LIST OF THE THREE PROJECTS</h1>
+                <p align="left">
+                <h2>Project-Two</h2>
+                This Project is about a Registration Form that i have completed in the guidence of professor at Cencol</p>
+                <img src="./images/project2.png" style="width:100%">
+                </p>
+
+                <p align="left">
+                <h2>Project-Three</h2>
+                This Project is a big project that i have completed as an assignment at Cencol
+                    <img src="./images/project3.png" style="width:100%">
+                    <h3>Above Mentioned Pictures are the Screenshot of the projects that I have Completed.</h3>
                     <br>
-                    <h2>Project-One</h2>
-                    This picture shows the pictures of the project that i have completed at Cencol.
-                    <img src="./images/project1.png"style="width:100%">
-                    
-                    </p>
-                    <p align="left">
-                   
-                    <h2>Project-Two</h2>
-                    This Project is about a Registration Form that i have completed in the guidence of professor at Cencol</p>
-                    <img src="./images/project2.png" style="width:100%">
-                    </p>
-                    <p align="left">
-                    
-                    <h2>Project-Three</h2>
-                    This Project is a big project that i have completed as an assignment at Cencol
-                        <img src="./images/project3.png" style="width:100%">
-                        <h3>Above Mentioned Pictures are the Screenshot of the projects that I have Completed.</h3>
-                        <br>
-                        <br>
-                    </p>
-                                        
-                `;
+                    <br>
+                </p>
+                                    
+            `;
             }
         });
     }
-  
-    function Start()
-    {
-      //  console.log('%cApp Started...', "color:white; font-size: 24px;");   
-
+    function Start() {
+        //  console.log('%cApp Started...', "color:white; font-size: 24px;");   
         InitializeSite();
-    } 
-
-
+    }
     window.addEventListener("load", Start);
-
 })();
--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,55 +1,47 @@
 // IIFE module
 "use strict";
 let objects;
-(function(objects)
-{
-    class Contact 
-    {
-        constructor(firstName="", lastName="", emailAddress="", contactNumber="", shortMessage="") 
-        {
+(function (objects) {
+    let Contact = /** @class */ (function () {
+        function Contact(firstName, lastName, emailAddress, contactNumber, shortMessage) {
+            if (firstName === void 0) { firstName = ""; }
+            if (lastName === void 0) { lastName = ""; }
+            if (emailAddress === void 0) { emailAddress = ""; }
+            if (contactNumber === void 0) { contactNumber = ""; }
+            if (shortMessage === void 0) { shortMessage = ""; }
             this.firstName = firstName;
             this.lastName = lastName;
             this.emailAddress = emailAddress;
             this.contactNumber = contactNumber;
             this.shortMessage = shortMessage;
-        } 
-
-        toString()
-        {
-            let outputstring = "";
-            outputstring += "First Name    : " + this.firstName + "\n";
-            outputstring += "Last Name     : " + this.lastName + "\n";
-            outputstring += "Email Address : " + this.emailAddress + "\n";
-            outputstring += "Contact Number: " + this.contactNumber + "\n";
-            outputstring += "Short Message : " + this.shortMessage + "\n";
-            return outputstring;
         }
-
-        toJSON()
-        {
-            let JSONObject =
-            {
+        Contact.prototype.toString = function () {
+            let outputstring = "";
+            outputstring += "Your First Name is    : " + this.firstName + "\n";
+            outputstring += "Your Last Name is     : " + this.lastName + "\n";
+            outputstring += "Your Email Address is : " + this.emailAddress + "\n";
+            outputstring += "Your Contact Number is: " + this.contactNumber + "\n";
+            outputstring += "Your Short Message is : " + this.shortMessage + "\n";
+            return outputstring;
+        };
+        Contact.prototype.toJSON = function () {
+            let JSONObject = {
                 firstName: this.firstName,
                 lastName: this.lastName,
                 emailAddress: this.emailAddress,
                 contactNumber: this.contactNumber,
-                shortMessage: this.shortMessage, 
+                shortMessage: this.shortMessage,
             };
-
             return JSONObject;
-        }
-
-        setContact(JSON_Data)
-        {
+        };
+        Contact.prototype.setContact = function (JSON_Data) {
             this.firstName = JSON_Data.firstName;
             this.lastName = JSON_Data.lastName;
             this.emailAddress = JSON_Data.emailAddress;
             this.contactNumber = JSON_Data.contactNumber;
             this.shortMessage = JSON_Data.shortMessage;
-        }
-
-
-    }
+        };
+        return Contact;
+    }());
     objects.Contact = Contact;
-
 }(objects || (objects = {})));
